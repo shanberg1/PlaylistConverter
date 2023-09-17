38f5c85685389807b14a707cf28b15c4a2637f60
shanberg1 <scotthanberg@gmail.com>
2023-09-16 20:19:54 -0400

    added description

--- frontend/src/Constants.ts
+++ frontend/src/Constants.ts
@@ -1,2 +1,4 @@
 const _debugMode = window.location.href.includes("localhost");
 export const _backendUrl = _debugMode ? "http://localhost:3000" : "https://playlist-converter.azurewebsites.net";
+
+export const InstructionText = "Just enter the url for a spotify or apple music playlist, click convert, and we will generate a new playlist on your chosen destination service for free! No authentication required."
