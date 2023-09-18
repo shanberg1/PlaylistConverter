81b01c793f79b604bd80ff22cd8596180cf90836
shanberg1 <scotthanberg@gmail.com>
2023-09-17 22:15:36 -0400

    added url validation

--- frontend/src/Constants.ts
+++ frontend/src/Constants.ts
@@ -2,3 +2,9 @@ const _debugMode = window.location.href.includes("localhost");
 export const _backendUrl = _debugMode ? "http://localhost:3000" : "https://playlist-converter.azurewebsites.net";
 
 export const InstructionText = "Just enter the url for a spotify or apple music playlist, click convert, and we will generate a new playlist on your chosen destination service for free! No authentication required."
+export const AppleMusicLibraryPlaylistValidationText = "An Apple Music library playlist is detected. Please make sure you are getting a catalog playlist instead. To ensure you are getting a catalog playlist, click \"Share\" and then \"Copy link to clipboard\"."
+export const GenericValidationText = "Your url is not formatted properly. Please double check it and try again."
+
+export enum URLExceptions {
+    APPLE_MUSIC_LIBRARY_PLAYLIST
+}
--- frontend/src/Controls/UrlTextbox.ts
+++ frontend/src/Controls/UrlTextbox.ts
@@ -1,9 +1,13 @@
+import { AppleMusicLibraryPlaylistValidationText, GenericValidationText, URLExceptions } from "../Constants";
 import { getElement } from "../HomePage";
 import { Media, Service, appleMusicAlbumRegex, appleMusicPlaylistRegex, appleMusicSongRegex } from "../Models/Models";
+import * as urlLib from "url";
 
 export class UrlTextbox {
     private urlString: string;
     private urlTextbox: HTMLInputElement;
+    private urlValidationMessage: HTMLInputElement;
+
     private service: Service;
     private typingTimer: NodeJS.Timeout;
     private callBack: (service: Service, media: Media) => void;
@@ -11,6 +15,8 @@ export class UrlTextbox {
 
     constructor(setSource: (service: Service, media: Media) => void) {
         this.urlTextbox = getElement("url");
+        this.urlValidationMessage = getElement("urlSublabel");
+        this.urlValidationMessage.textContent = "";
         document.querySelector("#url")?.addEventListener("keyup", () => this.onKeyUp());
         this.urlString = "";
         this.service = "None";
@@ -34,9 +40,7 @@ export class UrlTextbox {
     private onKeyUp() {
         console.log("keyup");
         clearTimeout(this.typingTimer);
-        if (this.urlTextbox.value) {
-            this.typingTimer = setTimeout(() => this.doneTyping(), 1000);
-        }
+        this.typingTimer = setTimeout(() => this.doneTyping(), 1000);
     }
 
     private doneTyping() {
@@ -44,6 +48,34 @@ export class UrlTextbox {
         this.urlString = this.urlTextbox.value;
         this.service = this.tryDetectService();
         this.media = this.tryDetectMediaType();
+        if (this.service === "AppleMusic") {
+            try {
+                this.validateAppleMusicURL(this.urlString);
+                this.urlValidationMessage.textContent = "";
+            } catch (err) {
+                if (err === URLExceptions.APPLE_MUSIC_LIBRARY_PLAYLIST) {
+                    this.urlValidationMessage.textContent = AppleMusicLibraryPlaylistValidationText;
+                } else {
+                    this.urlValidationMessage.textContent = GenericValidationText;
+                }
+                return;
+            }
+        } else if (this.service === "Spotify") {
+            try {
+                this.validateSpotifyURL(this.urlString);
+                this.urlValidationMessage.textContent = "";
+            } catch (err) {
+                this.urlValidationMessage.textContent = GenericValidationText;
+                return;
+            }
+        } else {
+            if (this.urlString) {
+                this.urlValidationMessage.textContent = this.urlValidationMessage.textContent = GenericValidationText;
+            } else {
+                this.urlValidationMessage.textContent = "";
+            }
+        }
+
         this.callBack(this.service, this.media);
     }
 
@@ -79,5 +111,41 @@ export class UrlTextbox {
         }
 
         return "None"
+    }
+
+    private validateAppleMusicURL(url: string): boolean {
+        const parsedUrl = urlLib.parse(url);
+        const path: string = parsedUrl.path;
+        const splitPath = path.split("/");
+
+        if (splitPath.find((pathElement) => pathElement.toLowerCase() === "library")) {
+            throw URLExceptions.APPLE_MUSIC_LIBRARY_PLAYLIST;
+        }
+
+        if (splitPath[2].toLowerCase() !== "playlist") {
+            throw "";
+        }
+
+        if (splitPath.length < 4) {
+            throw "";
+        }
+
+        return true;
+    }
+
+    private validateSpotifyURL(url: string): boolean {
+        const parsedUrl = urlLib.parse(url);
+        const path: string = parsedUrl.path;
+        const splitPath = path.split("/");
+
+        if (splitPath[1].toLowerCase() !== "playlist") {
+            throw "";
+        }
+
+        if (splitPath.length !== 3) {
+            throw "";
+        }
+
+        return true;
     }
 }
