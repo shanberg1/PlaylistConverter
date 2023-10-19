19e14a3f2350ebd109433aa89b90ef29884eb948
shanberg1 <scotthanberg@gmail.com>
2023-10-19 18:01:13 -0400

    fixed url

--- frontend/src/Constants.ts
+++ frontend/src/Constants.ts
@@ -1,5 +1,5 @@
 const _debugMode = window.location.href.includes("localhost");
-export const _backendUrl = _debugMode ? "http://localhost:3000" : "https://playlist-converter.azurewebsites.net";
+export const _backendUrl = _debugMode ? "http://localhost:3000" : "https://playlist-converter.com";
 
 export const InstructionText = "Just enter the url for a spotify or apple music playlist, click convert, and we will generate a new playlist on your chosen destination service for free! No authentication required."
 export const AppleMusicLibraryPlaylistValidationText = "An Apple Music library playlist is detected. Please make sure you are getting a catalog playlist instead. To ensure you are getting a catalog playlist, click \"Share\" and then \"Copy link to clipboard\"."
